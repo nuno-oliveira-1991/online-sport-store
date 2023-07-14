4067fbe99e101a55dbff7a39ebbe71dd828ebc90
Nuno Oliveira <nuno.oliveira.1991@gmail.com>
2023-07-14 11:17:06 +0100

    First commit

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,10 +1,10 @@
 import style from "./header.module.scss";
-import logoSrc from "../../assets/logo.svg";
-import Button from "../../UI/Button/Button";
+import logoSrc from "./../../../assets/logo.svg";
+import Button from "./../../UI/Button/Button";
 
 //Assets
-import hamburgerIcon from "../../assets/icons/hamburger-icon.svg";
-import searchIcon from "../../assets/icons/search-icon.svg";
+import hamburgerIcon from "./../../..//assets/icons/hamburger-icon.svg";
+import searchIcon from "./../../../assets/icons/search-icon.svg";
 
 const Header = () => {
 	return (
@@ -12,10 +12,6 @@ const Header = () => {
 			<header className={style.header}>
 				<div className={style.leftSide}>
 					<Button onlyIcon={true} text="Menu" icon={hamburgerIcon} />
-
-					<Button />
-
-					<Button iconRight={true} icon={searchIcon} />
 					<Button icon={searchIcon} />
 
 					<input type="text" placeholder="Search..." />
@@ -23,10 +19,13 @@ const Header = () => {
 				<div className={style.logo}>
 					<img src={logoSrc} alt="Logo" />
 				</div>
-				<div className={style.rightSide}>Secao 3</div>
+				<div className={style.rightSide}>
+					<Button />
+					<Button />
+				</div>
 			</header>
 		</>
 	);
-};
+}
 
 export default Header;
